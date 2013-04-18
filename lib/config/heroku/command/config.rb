d727eb26df3f92743779f698c2bfaff76dfe355c
Hassan Shahid <set5think@gmail.com>
2013-04-18 14:01:39 -0400

    specify config variables to copy

--- lib/config/heroku/command/config.rb
+++ lib/config/heroku/command/config.rb
@@ -18,6 +18,9 @@ class Heroku::Command::Config
         display("#{app} has no config vars.")
       else
         vars.each {|key, value| vars[key] = value.to_s}
+        unless args.empty?
+          vars = vars.select {|key, value| args.include?(key) }
+        end
         action("Setting config vars and restarting #{dst_app}") do
           api.put_config_vars(dst_app, vars)
           @status = begin
