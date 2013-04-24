a60f2780e5d455a4523cd4e0b627e2dd0d96736d
Hassan Shahid <set5think@gmail.com>
2013-04-24 17:37:27 -0400

    turn maintenance on for all apps with ALL arg

--- lib/config/heroku/command/maintenance.rb
+++ lib/config/heroku/command/maintenance.rb
@@ -1,5 +1,22 @@
-require 'heroku/command/config'
+require 'heroku/command/maintenance'
 
 class Heroku::Command::Maintenance
 
+  def on
+    if args.empty?
+      super
+    else
+      if args.include?('ALL')
+        apps - api.get_apps.body
+        unless apps.empty?
+          apps.each do |my_app|
+            validate_arguments!
+            action("Enabling maintenance mode for #{my_app}") do
+              api.post_appi_maintenance(my_app, '1')
+            end
+          end
+        end
+      end
+    end
+  end
 end
