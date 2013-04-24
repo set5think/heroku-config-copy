fc058f90d449b564798336973df7e16f04791257
Hassan Shahid <set5think@gmail.com>
2013-04-24 17:48:51 -0400

    need to grab the name from the json app object

--- lib/config/heroku/command/maintenance.rb
+++ lib/config/heroku/command/maintenance.rb
@@ -10,8 +10,8 @@ class Heroku::Command::Maintenance
         apps = api.get_apps.body
         unless apps.empty?
           apps.each do |my_app|
-            action("Enabling maintenance mode for #{my_app}") do
-              api.post_appi_maintenance(my_app, '1')
+            action("Enabling maintenance mode for #{my_app['name']}") do
+              api.post_appi_maintenance(my_app['name'], '1')
             end
           end
         end
@@ -27,8 +27,8 @@ class Heroku::Command::Maintenance
         apps = api.get_apps.body
         unless apps.empty?
           apps.each do |my_app|
-            action("Disabling maintenance mode for #{my_app}") do
-              api.post_appi_maintenance(my_app, '0')
+            action("Disabling maintenance mode for #{my_app['name']}") do
+              api.post_appi_maintenance(my_app['name'], '0')
             end
           end
         end
