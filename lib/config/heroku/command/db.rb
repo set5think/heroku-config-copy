756d163229710d0fe68c919ba8f22e6ae23ed4f5
Hassan Shahid <set5think@gmail.com>
2013-07-09 01:47:50 -0400

    now bringing it back fo real

--- lib/config/heroku/command/db.rb
+++ lib/config/heroku/command/db.rb
@@ -6,30 +6,34 @@ class Heroku::Command::Db
 
     db = args.detect { |a| a.include?('HEROKU_POSTGRESQL_') } || 'DATABASE_URL'
 
-    (db_name, db_string) = heroku.config_vars(app).select { |k, v| k.include?(db) }
+    db_info = {}
 
-    puts db_name
-    puts db_string
+    heroku.config_vars(app).select do |k, v|
+      if k.include?(db)
+        db_info[:name] = k
+        db_info[:conn_string] = v
+      end
+    end
 
-    # uri = URI.parse(db_string)
-    # uri_parts = {
-    #   :host   => uri.host,
-    #   :db     => cleanse_path(uri.path),
-    #   :user   => uri.user,
-    #   :pw     => uri.password,
-    #   :scheme => uri.scheme,
-    #   :port   => uri.port
-    # }
+    uri = URI.parse(db_info[:conn_string])
+    uri_parts = {
+      :host   => uri.host,
+      :db     => cleanse_path(uri.path),
+      :user   => uri.user,
+      :pw     => uri.password,
+      :scheme => uri.scheme,
+      :port   => uri.port
+    }
 
-    # return "#{uri_parts['scheme']} not supported yet" if uri_parts['scheme'] != 'postgres'
+    return "#{uri_parts['scheme']} not supported yet" if uri_parts['scheme'] != 'postgres'
 
-    # if parse_style.nil? || parse_style == "psql"
-    #   return "psql -h #{uri_parts['host']} -d #{uri_parts['db']} -U #{uri_parts['user']}"
-    # elsif parse_style == 'pgpass'
-    #   return "#{uri_parts['host']}:#{uri_parts['port']}:#{uri_parts['db']}:#{uri_parts['user']}:#{uri_parts['pw']}"
-    # else
-    #   return "#{parse_style} not known or supported. Please use 'psql' or 'pgpass'"
-    # end
+    if parse_style.nil? || parse_style == "psql"
+      return "psql -h #{uri_parts['host']} -d #{uri_parts['db']} -U #{uri_parts['user']}"
+    elsif parse_style == 'pgpass'
+      return "#{uri_parts['host']}:#{uri_parts['port']}:#{uri_parts['db']}:#{uri_parts['user']}:#{uri_parts['pw']}"
+    else
+      return "#{parse_style} not known or supported. Please use 'psql' or 'pgpass'"
+    end
   end
 
   protected
