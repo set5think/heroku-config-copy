b4471fb736c52ded851eeca3668e69509ad6ffec
Hassan Shahid <set5think@gmail.com>
2013-07-09 01:39:48 -0400

    seeing if db_string gets assigned correctly

--- lib/config/heroku/command/db.rb
+++ lib/config/heroku/command/db.rb
@@ -8,25 +8,28 @@ class Heroku::Command::Db
 
     db_name, db_string = heroku.config_vars(app).select { |k, v| k.include?(db) }
 
-    uri = URI.parse(db_string)
-    uri_parts = {
-      :host   => uri.host,
-      :db     => cleanse_path(uri.path),
-      :user   => uri.user,
-      :pw     => uri.password,
-      :scheme => uri.scheme,
-      :port   => uri.port
-    }
+    puts db_name
+    puts db_string
 
-    return "#{uri_parts['scheme']} not supported yet" if uri_parts['scheme'] != 'postgres'
+    # uri = URI.parse(db_string)
+    # uri_parts = {
+    #   :host   => uri.host,
+    #   :db     => cleanse_path(uri.path),
+    #   :user   => uri.user,
+    #   :pw     => uri.password,
+    #   :scheme => uri.scheme,
+    #   :port   => uri.port
+    # }
 
-    if parse_style.nil? || parse_style == "psql"
-      return "psql -h #{uri_parts['host']} -d #{uri_parts['db']} -U #{uri_parts['user']}"
-    elsif parse_style == 'pgpass'
-      return "#{uri_parts['host']}:#{uri_parts['port']}:#{uri_parts['db']}:#{uri_parts['user']}:#{uri_parts['pw']}"
-    else
-      return "#{parse_style} not known or supported. Please use 'psql' or 'pgpass'"
-    end
+    # return "#{uri_parts['scheme']} not supported yet" if uri_parts['scheme'] != 'postgres'
+
+    # if parse_style.nil? || parse_style == "psql"
+    #   return "psql -h #{uri_parts['host']} -d #{uri_parts['db']} -U #{uri_parts['user']}"
+    # elsif parse_style == 'pgpass'
+    #   return "#{uri_parts['host']}:#{uri_parts['port']}:#{uri_parts['db']}:#{uri_parts['user']}:#{uri_parts['pw']}"
+    # else
+    #   return "#{parse_style} not known or supported. Please use 'psql' or 'pgpass'"
+    # end
   end
 
   protected
